e12fce53dff94d89a310cf55575f93485822a6c8
jervdrdo <jeroen.vanderdonckt@ugent.be>
2021-06-05 18:37:58 +0200

    :fire: restructure processing classes

--- time_series/processing/__init__.py
+++ time_series/processing/__init__.py
@@ -2,12 +2,12 @@
 
 __author__ = "Jonas Van Der Donckt, Emiel Deprost, Jeroen Van Der Donckt"
 
-from .series_processor import SeriesProcessor, SeriesProcessorPipeline
-from .series_processor import dataframe_func  # , numpy_func
+from .series_processor import SeriesProcessor
+from .series_processor_pipeline import SeriesProcessorPipeline
+from .series_processor import dataframe_func
 
 __all__ = [
     "dataframe_func",
-    # "numpy_func",
     "SeriesProcessor",
     "SeriesProcessorPipeline",
 ]
