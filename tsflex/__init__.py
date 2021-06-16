551328d4adf40ff3640a42329df6d8bc0a1433bc
jonas <jonvdrdo.vanderdonckt@ugent.be>
2021-06-16 21:21:11 +0200

    :book: import structure is more logical now

--- tsflex/__init__.py
+++ tsflex/__init__.py
@@ -7,50 +7,50 @@ __docformat__ = 'numpy'
 __author__ = "Jonas Van Der Donckt, Jeroen Van Der Donckt, Emiel Deprost"
 __pdoc__ = {
     # do not show tue utils module
-    # 'tsflex.utils': False,
+    'tsflex.utils': False,
     # show the seriesprocessor & numpyfuncwrapper their call method
     'SeriesProcessor.__call__': True,
     'NumpyFuncWrapper.__call__': True,
 }
 
 
-from .processing import (
-    SeriesProcessor,
-    SeriesPipeline,
-    SKSeriesPipeline,
-    dataframe_func,
-    get_processor_logs,
-)
-
-from .features import (
-    FeatureDescriptor,
-    MultipleFeatureDescriptors,
-    FeatureCollection,
-    SKFeatureCollection,
-    NumpyFuncWrapper,
-    get_feature_logs,
-)
-
-from .chunking import chunk_data
+# from .processing import (
+#     SeriesProcessor,
+#     SeriesPipeline,
+#     SKSeriesPipeline,
+#     dataframe_func,
+#     get_processor_logs,
+# )
+#
+# from .features import (
+#     FeatureDescriptor,
+#     MultipleFeatureDescriptors,
+#     FeatureCollection,
+#     SKFeatureCollection,
+#     NumpyFuncWrapper,
+#     get_feature_logs,
+# )
+#
+# from .chunking import chunk_data
 
 __all__ = [
-    # Processing
-    "SeriesProcessor",
-    "dataframe_func",
-    "SeriesPipeline",
-    "SKSeriesPipeline",
-    "get_processor_logs",
-
-    # Features
-    "NumpyFuncWrapper",
-    "FeatureDescriptor",
-    "MultipleFeatureDescriptors",
-    "FeatureCollection",
-    "SKFeatureCollection",
-    "get_feature_logs",
+    # # Processing
+    # "SeriesProcessor",
+    # "dataframe_func",
+    # "SeriesPipeline",
+    # "SKSeriesPipeline",
+    # "get_processor_logs",
+    #
+    # # Features
+    # "NumpyFuncWrapper",
+    # "FeatureDescriptor",
+    # "MultipleFeatureDescriptors",
+    # "FeatureCollection",
+    # "SKFeatureCollection",
+    # "get_feature_logs",
 
     # Chunking
-    "chunk_data",
+    # "chunk_data",
 
     # documentation
     "__pdoc__"
