95f50028c3f16196f855f45869a67f0b137c7646
jervdrdo <jeroen.vanderdonckt@ugent.be>
2021-07-29 00:17:22 +0200

    :sparkles: add integrations

---
+++ tsflex/features/integrations.py
@@ -0,0 +1,123 @@
+"""Wrappers for seamless integration of feature functions from other packages."""
+
+__author__ = "Jeroen Van Der Donckt, Jonas Van Der Donckt, Emiel Deprost"
+
+import pandas as pd
+import numpy as np
+
+from typing import Callable, Any, Optional, List, Dict
+from .feature import FuncWrapper
+
+
+def get_name(func: Callable) -> str:
+    """Get the name of the function.
+
+    Parameters
+    ----------
+    func: Callable
+        The function whose name has to be returned, should be either a function or an
+        object that is callable.
+
+    Returns
+    -------
+    str
+        The name of ``func`` in case of a function or the name of the class in case
+        of a callable object.
+
+    """
+    assert callable(func), f"The given argument {func} is not callable!"
+    try:
+        return func.__name__
+    except:
+        return type(func).__name__
+
+# TODO: hoort dit hier?
+def make_robust(
+    func: Callable,
+    min_nb_samples: int = 1,
+    error_val: Any = np.nan,
+    output_names = None,
+    **kwargs,
+) -> FuncWrapper:
+    def wrap_func(x: np.ndarray):
+        if len(x) < min_nb_samples:
+            return error_val
+        return func(x)
+
+    wrap_func.__name__ = "[robust]__" + get_name(func)
+    output_names = get_name(func) if output_names is None else output_names
+    return FuncWrapper(wrap_func, output_names=output_names, **kwargs)
+
+
+## SEGLEARN
+
+# Decorator for seglearn functions doing the conversion (mentioned above)
+def seglearn_wrapper(func: Callable, output_names: Optional[str] = None) -> FuncWrapper:
+    """Wrapper enabling compatibility with seglearn functions.
+    
+    As [seglearn feature-functions](https://github.com/dmbee/seglearn/blob/master/seglearn/feature_functions.py) 
+    are vectorized along the first axis (axis=0), we need to expand our window-data.  
+    This wrapper converts `1D np.array` to a `2D np.array` with all the window-data in 
+    `axis=1`.
+
+    Parameters
+    ----------
+    func: Callable
+        The seglearn function.
+    output_names: str, optional
+        The output name for the function its output.
+
+    Returns
+    -------
+    FuncWrapper
+        The wrapped seglearn function that is compatible with tsflex.
+
+    """
+    def wrap_func(x: np.ndarray):
+        out = func(x.reshape(1, len(x)))
+        return out.flatten()
+
+    wrap_func.__name__ = "[seglearn_wrapped]__" + get_name(func)
+    output_names = get_name(func) if output_names is None else output_names
+    return FuncWrapper(wrap_func, output_names=output_names)
+
+
+## TSFRESH
+
+
+def tsfresh_combiner_wrapper(func: Callable, param: List[Dict]) -> FuncWrapper:
+    """Wrapper enabling compatibility with tsfresh combiner functions.
+
+    [tsfresh feature-funtions](https://github.com/blue-yonder/tsfresh/blob/main/tsfresh/feature_extraction/feature_calculators.py) are either of type `simple` or `combiner`.
+    * `simple`: feature calculators which calculate a single number  
+      **=> integrates natively with tsflex**
+    * `combiner`: feature calculates which calculate a bunch of features for a list of parameters. 
+       These features are returned as a list of (key, value) pairs for each input parameter.  
+       **=> requires wrapping the function to only extract the values of the returned tuples**  
+
+    Parameters
+    ----------
+    func: Callable
+        The tsfresh combiner function.
+    param: List[Dict]
+        List containing dictionaries with the parameter(s) for the combiner function.
+        This is exactly the same ``param`` as you would pass to a tsfresh combiner 
+        function.
+
+    Returns
+    -------
+    FuncWrapper
+        The wrapped tsfresh combiner function that is compatible with tsflex.
+
+    """
+    def wrap_func(x: np.ndarray):
+        out = func(x, param)
+        return tuple(t[1] for t in out)
+
+    wrap_func.__name__ = "[tsfresh-combiner_wrapped]__" + get_name(func)
+    input_type = pd.Series if hasattr(func, "index_type") else np.array
+    return FuncWrapper(
+        wrap_func,
+        output_names=[func.__name__ + "_" + str(p) for p in param],
+        input_type=input_type,
+    )
