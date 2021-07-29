d241986b67f140a72ab913721624d4c2348c90a7
jervdrdo <jeroen.vanderdonckt@ugent.be>
2021-07-29 14:34:45 +0200

    :fire: add multi_input test for make_robust

--- tests/test_features_integration.py
+++ tests/test_features_integration.py
@@ -63,6 +63,31 @@ def test_robust_gap_features(dummy_data):
     assert res_df.isna()[1:4].all().all()
     assert res_df[4:].isna().any().any() == False
 
+
+def test_robust_gap_features_multi_input(dummy_data):
+    def abs_diff_mean(x: np.ndarray, y: np.ndarray):
+        abs_diff = np.abs(x - y)
+        return np.mean(abs_diff)
+    
+    feats = MultipleFeatureDescriptors(
+        functions=make_robust(abs_diff_mean, min_nb_samples=2),
+        series_names=("EDA", "TMP"),
+        windows="10s",
+        strides="5s",
+    )
+    feature_collection = FeatureCollection(feats)
+
+    eda_tmp_data = dummy_data[["EDA", "TMP"]].dropna()
+    eda_tmp_data[2 : 1 + 25 * 4] = None  # Leave gap of 25 s
+    eda_tmp_data = eda_tmp_data.dropna()
+    assert eda_tmp_data.isna().any().any() == False
+    assert (eda_tmp_data.index[1:] - eda_tmp_data.index[:-1]).max() == pd.Timedelta("25 s")
+
+    res_df = feature_collection.calculate(eda_tmp_data, return_df=True, approve_sparsity=True)
+    assert res_df.isna()[1:4].all().all()
+    assert res_df[4:].isna().any().any() == False
+
+
 def test_robust_gap_features_multi_output(dummy_data):
     def mean_std(x: np.ndarray):
         return np.mean(x), np.std(x)
