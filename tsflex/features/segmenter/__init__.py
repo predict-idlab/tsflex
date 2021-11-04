ae8aa9ae8db223f4e5cc7ee011b3af4f92f1d4ed
jonvdrdo <jonvdrdo.vanderdonckt@ugent.be>
2021-11-04 11:28:20 +0100

    :see_no_evil: removing unnecesarry import

--- tsflex/features/segmenter/__init__.py
+++ tsflex/features/segmenter/__init__.py
@@ -3,7 +3,6 @@
 
 __author__ = "Jonas Van Der Donckt"
 
-from ... import __pdoc__
 from .strided_rolling import StridedRolling #TimeStridedRolling, SequenceStridedRolling
 from .strided_rolling_factory import StridedRollingFactory
 
