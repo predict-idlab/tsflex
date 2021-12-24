a59ebfe815211267dbe7e1371e49341651e968ad
jervdrdo <jeroen.vanderdonckt@ugent.be>
2021-12-24 11:28:32 +0100

    :rocket: deploy new version

--- tsflex/__init__.py
+++ tsflex/__init__.py
@@ -9,7 +9,7 @@
 
 __docformat__ = 'numpy'
 __author__ = "Jonas Van Der Donckt, Jeroen Van Der Donckt, Emiel Deprost"
-__version__ = '0.2.3.1'
+__version__ = '0.2.3.3'
 __pdoc__ = {
     # do not show tue utils module
     'tsflex.utils': False,
