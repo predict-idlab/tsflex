c12b9c9eecf6de91bbf8f4282af262fd1fe93729
jonvdrdo <jonvdrdo.vanderdonckt@ugent.be>
2022-02-08 18:58:30 +0100

    :rocket:

--- tsflex/__init__.py
+++ tsflex/__init__.py
@@ -9,7 +9,7 @@
 
 __docformat__ = 'numpy'
 __author__ = "Jonas Van Der Donckt, Jeroen Van Der Donckt, Emiel Deprost"
-__version__ = '0.2.3.5'
+__version__ = '0.2.3.6'
 __pdoc__ = {
     # do not show tue utils module
     'tsflex.utils': False,
