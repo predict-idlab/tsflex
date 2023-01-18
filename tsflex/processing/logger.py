3c65d360bb125d22e55fd44c9fe103264cf50c82
Jeroen Van Der Donckt <boebievdd@gmail.com>
2023-01-18 11:55:11 +0100

    :pen: review code

--- tsflex/processing/logger.py
+++ tsflex/processing/logger.py
@@ -6,7 +6,7 @@ SeriesPipeline : its `logging_file_path` of the `process` method.
 
 """
 
-__author__ = "Jeroen Van Der Donckt"
+__author__ = "Jeroen Van Der Donckt, Jonas Van Der Donckt"
 
 import logging
 import pandas as pd
@@ -37,24 +37,24 @@ def _parse_message(message: str) -> list:
 
 
 def _parse_logging_execution_to_df(logging_file_path: str) -> pd.DataFrame:
-    """Parse the logged messages into a dataframe that contains execution info.
+    """Parse the logged messages into a DataFrame that contains execution info.
 
     Parameters
     ----------
     logging_file_path: str
         The file path where the logged messages are stored. This is the file path that
-        is passed to the ``SeriesPipeline`` its process method.
+        is passed to the ``SeriesPipeline`` its ``process`` method.
 
     Returns
     -------
     pd.DataFrame
-        A DataFrame with the processor its method, series names and calculation 
-        duration.
+        A DataFrame with the processor its method, series names, output names, and
+        calculation duration.
 
     Note
     ----
-    This function only works when the ``logging_file_path`` used in a
-    ``SeriesPipeline`` its ``process`` method is passed.
+    This function only works when the ``logging_file_path`` used in a ``SeriesPipeline``
+    its ``process`` method is passed.
 
     """
     df = logging_file_to_df(logging_file_path)
@@ -78,7 +78,8 @@ def get_processor_logs(logging_file_path: str) -> pd.DataFrame:
     Returns
     -------
     pd.DataFrame
-        A DataFrame containing each processor its duration and its series names.
+        A DataFrame containing each processor its series names, output names, and
+        duration.
     
     """
     df = _parse_logging_execution_to_df(logging_file_path)
